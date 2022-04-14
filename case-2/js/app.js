2c40ef3f8661936a7db787488569a6e5c9cc32bd
Shaktyka <k-i-s-u-n-j-a@list.ru>
2022-04-14 20:20:18 +0500

    Стало скучно, затащила реакт в проект

---
+++ case-2/js/app.js
@@ -0,0 +1,107 @@
+
+// Карточка товара:
+const Card = ({id, title, article, rating, reviews, price, image}) => {
+    return (
+        <li className="cards__item card" tabindex="0" id={id}>
+            <span className="label label--popular card__label">
+                Новинка
+            </span>
+            <span className="label label--sale card__label">
+                Скидка 50%
+            </span>
+            <span className="label label--hit card__label">
+                Хит
+            </span>
+            <div className="card__img-wrap">
+                <img 
+                    className="img card__img" 
+                    src={image}
+                    width="270" height="200"
+                    alt={title}
+                />
+            </div>
+            <div className="card__description">
+                <div className="card__info">
+                    <span className="card__articul">Артикул: {article}</span>
+                    <div className="card__rating">
+                        <span className="card__stars">
+                            <img 
+                                className="card__star" 
+                                src="img/star.svg" 
+                                width="15" height="15"
+                                alt="Звезд рейтинга"
+                            />{rating}
+                        </span>
+                        <a className="card__reviews" href="#">{reviews} отзывов</a>
+                    </div>
+                </div>
+                <h3 className="card__title">{title}</h3>
+                <div className="card__buy">
+                    <div className="card__prices">
+                        <span className="card__price">{price} &#8381;</span>
+                        <span className="card__price card__price--old">435 &#8381;</span>
+                    </div>
+                    <div className="card__buttons">
+                        <button className="button card__button" type="button">
+                            <img 
+                                className="button__img" src="img/heart.svg" 
+                                width="20px" height="20px" 
+                                alt="Кнопка 'В избранное'"
+                            />
+                            <span className="visually-hidden">Добавить в избранное</span>
+                        </button>
+                        <button className="button card__button card__button--buy" type="button">
+                            <img 
+                                className="button__img" src="img/cart.svg" 
+                                width="20px" height="20px" 
+                                alt="Кнопка 'В корзину'"
+                            />
+                            <span className="visually-hidden">Добавить в корзину</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </li>
+    );
+}
+
+// Список карточек
+const CardsList = ({cards}) => {
+
+    return (
+        <section className="cards">
+            <ul className="list cards__list">
+                <li>тест</li>
+            </ul>
+        </section>
+    );
+};
+
+// Контейнер страницы
+const Main = ({cards}) => {
+    return (
+        <main className="main">
+            <div className="container">
+                <CardsList cards={cards} />
+            </div>
+        </main>
+    );
+};
+
+// Тестовый элемент
+const PageTitle = () => {
+    return (
+        <h1>Каталог</h1>
+    );
+}
+
+// Приложение
+const App = () => {
+    return (
+        <Main cards={cards} />
+    );
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
---
+++ case-2/js/data.js
@@ -0,0 +1,114 @@
+const cards = [
+   {
+        id: 1,
+        title: 'Набор «Я – доктор»',
+        article: 856935,
+        rating: 4.9,
+        reviews: 19,
+        price: 590,
+        image: 'img-1.jpeg'
+   },
+   {
+      id: 2,
+      title: 'Деревянная игрушка «Змейка»',
+      article: 894935,
+      rating: 4.8,
+      reviews: 35,
+      price: 290,
+      image: 'img-2.jpeg',
+      hasSale50: true
+   },
+   {
+      id: 3,
+      title: 'Деревянная железная дорога с паровозиками',
+      article: 957935,
+      rating: 4.9,
+      reviews: 45,
+      price: 990,
+      image: 'img-3.jpeg'
+   },
+   {
+      id: 4,
+      title: 'Игрушка «Робот» заводная механическая',
+      article: 856989,
+      rating: 4.7,
+      reviews: 18,
+      price: 290,
+      image: 'img-4.jpeg'
+   },
+   {
+      id: 5,
+      title: 'Мягкий конструктор блочный, 50 деталей',
+      article: 866935,
+      rating: 4.9,
+      reviews: 65,
+      price: 790,
+      image: 'img-5.jpeg'
+   },
+   {
+      id: 6,
+      title: 'Развивающая деревянная игрушка «Ёжик»',
+      article: 756935,
+      rating: 4.9,
+      reviews: 34,
+      price: 390,
+      image: 'img-6.jpeg'
+   },
+   {
+      id: 7,
+      title: 'Деревянная игрушка неваляшка «Зайчик»',
+      article: 856777,
+      rating: 5.0,
+      reviews: 5,
+      price: 490,
+      image: 'img-7.jpeg',
+      isHit: true
+   },
+   {
+      id: 8,
+      title: 'Деревянная головоломка «Черепаха»',
+      article: 859935,
+      rating: 4.7,
+      reviews: 17,
+      price: 390,
+      image: 'img-8.jpeg'
+   },
+   {
+      id: 9,
+      title: 'Деревянный пазл',
+      article: 853335,
+      rating: 4.8,
+      reviews: 18,
+      price: 290,
+      image: 'img-9.jpeg'
+   },
+   {
+      id: 10,
+      title: 'Кубик рубика 3х3',
+      article: 866635,
+      rating: 4.5,
+      reviews: 12,
+      price: 390,
+      image: 'img-10.jpeg'
+   },
+   {
+      id: 11,
+      title: 'Счеты деревянные',
+      article: 222935,
+      rating: 4.9,
+      reviews: 7,
+      price: 390,
+      image: 'img-11.jpeg'
+   },
+   {
+      id: 12,
+      title: 'Мягкая игрушка «Овечка»',
+      article: 899935,
+      rating: 5.0,
+      reviews: 5,
+      price: 690,
+      image: 'img-12.jpeg',
+      isNew: true
+   },
+   
+];
