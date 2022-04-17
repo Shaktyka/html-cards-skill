14a943ae3f880962fc7380a756849acaab3bc17f
Shaktyka <k-i-s-u-n-j-a@list.ru>
2022-04-17 10:13:43 +0500

    Добавляет данные для карточек и вёрстку самой карточки

--- case-4/js/app.js
+++ case-4/js/app.js
@@ -1,3 +1,43 @@
+
+// Карточка товара:
+const Card = (props) => {
+    const {id, title, rating, reviews, price, salePrice, images, isAvailable, sale, isHit, cashback } = props.data;
+
+    const imgUrl = `img/${images[0]}`;
+    const className = `cards__item card`;
+
+    return (
+        <li className={className} tabIndex="0" id={id}>
+            карточка
+        </li>
+    );
+};
+
+// Список карточек
+const CardsList = ({cards}) => {
+
+    return (
+        <section className="cards">
+            <ul className="list cards__list">
+            {
+                cards.map((dataObj) => <Card key={dataObj.id} data={dataObj} />)
+            }
+            </ul>
+        </section>
+    );
+};
+
+// Контейнер страницы
+const Main = ({cards}) => {
+    return (
+        <main className="main">
+            <div className="container">
+                <CardsList cards={cards} />
+            </div>
+        </main>
+    );
+};
+
 // Тестовый элемент
 const PageTitle = () => {
     return (
@@ -6,12 +46,13 @@ const PageTitle = () => {
 }
 
 // Приложение
-const App = () => {
+const App = ({cards}) => {
     return (
-        <PageTitle />
+        <Main cards={cards} />
     );
 };
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<App cards={data} />);
+ 
---
+++ case-4/js/data.js
@@ -0,0 +1,114 @@
+const data = [
+    {
+        id: 1,
+        images: ['photo-1.png'],
+        price: 119990,
+        title: 'Смартфон APPLE IPhone 12 Pro Max 256Gb, черный',
+        isAvailable: true,
+        rating: 4.9,
+        reviews: 56
+    },
+    {
+        id: 2,
+        images: ['photo-2.png'],
+        price: 116990,
+        title: 'Смартфон APPLE Iphone 11 Pro  512Gb, черный',
+        isAvailable: true,
+        rating: 4.8,
+        reviews: 256
+    },
+    {
+        id: 3,
+        images: ['photo-3.png'],
+        price: 46990,
+        salePrice: 35990,
+        title: 'Смартфон GOOGLE Pixel 4a 128Gb, черный',
+        isAvailable: true,
+        rating: 4.2,
+        reviews: 155,
+        sale: 30
+    },
+    {
+        id: 4,
+        images: ['photo-4.png'],
+        price: 57990,
+        title: 'Смартфон OnePlus 8 Pro 128Gb, черный',
+        isAvailable: true,
+        rating: 4.3,
+        reviews: 76
+    },
+    {
+        id: 5,
+        images: ['photo-5.png'],
+        price: 57890,
+        title: 'Смартфон Sumsung Galaxy Note20 5G 256Gb, черный',
+        isAvailable: true,
+        rating: 5.0,
+        reviews: 46,
+        isHit: true
+    },
+    {
+        id: 6,
+        images: ['photo-6.png'],
+        price: 78990,
+        title: 'Смартфон Samsung Galaxy Note20 Ultra 5G 256Gb, черный',
+        isAvailable: true,
+        rating: 4.6,
+        reviews: 32
+    },
+    {
+        id: 7,
+        images: ['photo-7.png'],
+        price: 79990,
+        title: 'Смартфон Sony Xperia 1 II 256Gb, черный',
+        isAvailable: true,
+        rating: 4.2,
+        reviews: 53,
+        cashback: 30
+    },
+    {
+        id: 8,
+        images: ['photo-8.png'],
+        price: 57990,
+        title: 'Планшет APPLE IPud Air 4 64GB, серый',
+        isAvailable: true,
+        rating: 4.7,
+        reviews: 99
+    },
+    {
+        id: 9,
+        images: ['photo-9.png'],
+        price: 69990,
+        title: 'Смартфон APPLE iPad pro 11 128Gb, черный',
+        isAvailable: true,
+        rating: 4.7,
+        reviews: 112
+    },
+    {
+        id: 10,
+        images: ['photo-10.png'],
+        price: 119990,
+        title: 'Смартфон APPLE IPhone 12 Pro 256Gb, золотой',
+        isAvailable: true,
+        rating: 4.9,
+        reviews: 12
+    },
+    {
+        id: 11,
+        images: ['photo-11.png'],
+        price: 119990,
+        title: 'Смартфон APPLE IPhone 12 Pro 256Gb, графит',
+        isAvailable: true,
+        rating: 4.6,
+        reviews: 20
+    },
+    {
+        id: 12,
+        images: ['photo-12.png'],
+        price: 119990,
+        title: 'Смартфон APPLE IPhone 12 Pro 256Gb, голубой',
+        isAvailable: true,
+        rating: 4.9,
+        reviews: 51
+    }
+];
